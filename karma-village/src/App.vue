278adc99eca381aca8785296cdb7774700987bf1
ThVanC <thomas.vancleemput@ae.be>
2018-10-27 11:49:08 +0200

    create different pages

--- karma-village/src/App.vue
+++ karma-village/src/App.vue
@@ -1,38 +1,28 @@
 <template>
   <v-app>
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-toolbar>
-
-    <v-content>
-      <HelloWorld/>
+    <header-component></header-component>
+    <v-content>  
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/quest">Quest</router-link> |
+        <router-link to="/confirmation">Confirmation</router-link>
+      </div>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld'
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+import HeaderComponent from '@/components/Shared/Header.vue';
 
-export default {
-  name: 'App',
+@Component({
   components: {
-    HelloWorld
+    HeaderComponent,
   },
-  data () {
-    return {
-      //
-    }
-  }
+})
+export default class App extends Vue {
+
 }
+
 </script>
---
+++ karma-village/src/components/Shared/Header.vue
@@ -0,0 +1,29 @@
+<template>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Karma village</span>
+        <span class="font-weight-light">Technovate</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/AE-nv/Technovate"
+        target="_blank"
+      >
+        <span class="mr-2">Our github repository</span>
+      </v-btn>
+    </v-toolbar>
+</template>
+
+<script lang="ts">
+import {Vue, Component} from 'vue-property-decorator';
+
+@Component({})
+export default class HeaderComponent extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
