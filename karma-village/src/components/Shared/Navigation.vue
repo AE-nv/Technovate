02d234a92ac9407fc80916fe2cca8a29c34549bd
aylacremmeryae <ayla.cremmery@ae.be>
2018-11-08 15:35:06 +0100

    Removed inline styling

--- karma-village/src/components/Shared/Navigation.vue
+++ karma-village/src/components/Shared/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
     <v-toolbar app full-width>
-        <v-toolbar-items style="padding: 0; display: flex; width: 100%">
+        <v-toolbar-items>
             <slot></slot>
         </v-toolbar-items>
     </v-toolbar>
@@ -14,12 +14,16 @@ export default class Navigation extends Vue {}
 </script>
 
 <style lang="scss">
+
 .v-toolbar__content {
   width: 100%;
   display: flex;
   padding: 0;
   .v-toolbar__items {
     justify-content: space-between;
+    padding: 0; 
+    display: flex; 
+    width: 100%
   }
 }
 
